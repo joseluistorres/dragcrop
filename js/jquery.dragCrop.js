271248ae1122e5de0fcd70cb158135e89d18e9df
JoseLuis Torres <joseluis@powhow.com>
2012-12-11 22:08:30 -0600

    fixes to use more than one control

--- js/jquery.dragCrop.js
+++ js/jquery.dragCrop.js
@@ -25,7 +25,10 @@
 			view_port_width: 208,
 			view_port_height: 255,
 			ratio: 1,
-			form_id: 'frm_id_123456'
+			form_id: 'frm_id_123456',
+			max_left_image: 0,
+			max_top_image: 0,
+			crop_input: null
 		}; 
 			
 		var options = $.extend(defaults, options); 
@@ -33,32 +36,32 @@
 	   this.each(function() {
 			var x = 0; 
 		    var y = 0;
-			max_left_image = options.image_width - options.view_port_width;
-		    max_top_image = options.image_height - options.view_port_height;
+			options.max_left_image = options.image_width - options.view_port_width;
+		    options.max_top_image = options.image_height - options.view_port_height;
 		
 			$(this).css('position', 'relative');
  			// create the element
-			$('<div class="cropper_view_port">').appendTo($(this));
-			$('<input type="button">').attr('id', 'crop_' + options.image_id).appendTo($('div.cropper_view_port'));
-			crop_input = $('input#crop_' + options.image_id);
-			crop_input.css('background', "url('" + options.src_image + "')");
-			crop_input.css('background-size', '100%');
-			crop_input.css('width', options.image_width + 'px');
-			crop_input.css('height', options.image_height + 'px'); 
-			crop_input.css('position', 'absolute');
+			$('<div id="cropper_view_port_' + options.image_id + '">').appendTo($(this));
+			$('<input type="button">').attr('id', 'crop_' + options.image_id).appendTo($('div#cropper_view_port_' + options.image_id));
+			options.crop_input = $('input#crop_' + options.image_id);
+			options.crop_input.css('background', "url('" + options.src_image + "')");
+			options.crop_input.css('background-size', '100%');
+			options.crop_input.css('width', options.image_width + 'px');
+			options.crop_input.css('height', options.image_height + 'px'); 
+			options.crop_input.css('position', 'absolute');
 			left_position = - (options.image_width - options.view_port_width)/2;
 			top_position = - (options.image_height - options.view_port_height)/2; 
-			crop_input.css('left', left_position + 'px'); 
-			crop_input.css('top', top_position + 'px'); 
-			crop_input.css('cursor', 'move');
+			options.crop_input.css('left', left_position + 'px'); 
+			options.crop_input.css('top', top_position + 'px'); 
+			options.crop_input.css('cursor', 'move');
 			
 			
 			// cropper_view_port
-			$('div.cropper_view_port').css('position', 'relative');
-			$('div.cropper_view_port').css('width', options.view_port_width + 'px');
-			$('div.cropper_view_port').css('height', options.view_port_height + 'px');
-			$('div.cropper_view_port').css('z-index', '310');
-			$('div.cropper_view_port').css('overflow', 'hidden');
+			$('div#cropper_view_port_' + options.image_id).css('position', 'relative');
+			$('div#cropper_view_port_' + options.image_id).css('width', options.view_port_width + 'px');
+			$('div#cropper_view_port_' + options.image_id).css('height', options.view_port_height + 'px');
+			$('div#cropper_view_port_' + options.image_id).css('z-index', '310');
+			$('div#cropper_view_port_' + options.image_id).css('overflow', 'hidden');
 			
 			// create buttons zoom in and out
 			save_cropt_container = 'save_crop_container_' + options.image_id;
@@ -96,17 +99,17 @@
 			// enable zoom in
 			zoom_in.live('click', function(e){
 		        original_width = 1569;
-		        crop = crop_input;
+		        crop = options.crop_input;
 		        width = crop.width();
 		        height = crop.height();
 		        percentage_to_reduce = (100)/parseInt(width);
 		        reduce_amount = parseInt(parseInt(height) * percentage_to_reduce);
 		        crop.css('width', (parseInt(width) + 100) + 'px');
 		        crop.css('height', (parseInt(height) + reduce_amount - 9) + 'px');
-		        ratio = original_width/crop.width();
+		        options.ratio = original_width/crop.width();
 
-		        max_left_image = crop.width() - 208;
-		        max_top_image = crop.height() - 255;
+		        options.max_left_image = crop.width() - 208;
+		        options.max_top_image = crop.height() - 255;
 		        crop.css('image-rendering','-moz-crisp-edges');
 		        crop.css('image-rendering','auto');
 		        crop.css('left', parseFloat(crop.css("left")) + 1);
@@ -117,7 +120,7 @@
 		
 			zoom_out.live('click', function(e){
 		        original_width = 1569;
-		        crop = crop_input;
+		        crop = options.crop_input;
 		        width = crop.width();
 		        height = crop.height();
 		        percentage_to_reduce = (100)/parseInt(width);
@@ -126,10 +129,10 @@
 		        if ((parseInt(width) - 100)>=options.view_port_width && (parseInt(height) - reduce_amount - 9)>=options.view_port_height){
 		            crop.css('width', (parseInt(width) - 100) + 'px');
 		            crop.css('height', (parseInt(height) - reduce_amount - 9) + 'px');
-		            ratio = original_width/crop.width();
+		            options.ratio = original_width/crop.width();
 
-		            max_left_image = crop.width() - options.view_port_width;
-		            max_top_image = crop.height() - options.view_port_height;
+		            options.max_left_image = crop.width() - options.view_port_width;
+		            options.max_top_image = crop.height() - options.view_port_height;
 		            crop.css('image-rendering','-moz-crisp-edges');
 		            crop.css('image-rendering','auto');
 		            crop.css('left', '-49px');
@@ -140,7 +143,7 @@
 		    });
 		
 			//draggable
-			crop_input.mousedown(function () {
+			options.crop_input.mousedown(function () {
 
 		        var crop = $(this);
 
@@ -165,7 +168,7 @@
 
 		            var left = parseFloat(crop.css("left")) + x_movement;
 		            var min_left = 0;
-		            var max_left = - max_left_image;
+		            var max_left = - options.max_left_image;
 
 		            if (left >= min_left) left = min_left;
 		            if (left <= max_left) left = max_left;
@@ -173,17 +176,17 @@
 
 		            var top = parseFloat(crop.css("top")) + y_movement;
 		            var min_top = 0;
-		            var max_top =  - max_top_image;
+		            var max_top =  - options.max_top_image;
 
 		            if (top >= min_top) top = min_top;
 		            if (top <= max_top) top = max_top;
 		            crop.css("top", top);
 		            //Coordinates
 		            frm_str = $("form#" + options.form_id);
-		            frm_str.find("input#crop_x").val(Math.round(parseFloat(crop.css("left")) * -1 * ratio ));
-		            frm_str.find("input#crop_y").val(Math.round(parseFloat(crop.css("top")) * -1 * ratio ));
-		            frm_str.find("input#crop_w").val(Math.round(options.view_port_width * ratio));
-		            frm_str.find("input#crop_h").val(Math.round(options.view_port_height * ratio));
+		            frm_str.find("input#crop_x").val(Math.round(parseFloat(crop.css("left")) * -1 * options.ratio ));
+		            frm_str.find("input#crop_y").val(Math.round(parseFloat(crop.css("top")) * -1 * options.ratio ));
+		            frm_str.find("input#crop_w").val(Math.round(options.view_port_width * options.ratio));
+		            frm_str.find("input#crop_h").val(Math.round(options.view_port_height * options.ratio));
 
 		            //for demo
 		            $('div#show_crop_x').html('x=' + frm_str.find("input#crop_x").val());
